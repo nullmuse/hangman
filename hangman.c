9fd2eccfdbdb1f9de5cae5cf18126314f26acf44
nullmuse <dylan.liebgott@gmail.com>
2016-11-04 07:39:49 -0400

    Add game graphics, avatar generation

--- hangman.c
+++ hangman.c
@@ -1,15 +1,71 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
+#include <time.h>
+
+int draw_head(WINDOW *w) { 
+wattroff(w, COLOR_PAIR(1));
+int i; 
+          mvwprintw(w, 2, 12, "%s", "-----");
+          mvwprintw(w, 3, 12, "%s", "|O o|");
+          mvwprintw(w, 4, 12, "%s", "| ~ |"); 
+          mvwprintw(w, 5, 12, "%s", "-----");
+
+      wrefresh(w); 
+      wattron(w, COLOR_PAIR(1));
+
+      return 5;
+
+}
+
+int draw_part(WINDOW *w, int part, int loc) { 
+int i,min; 
+wattroff(w, COLOR_PAIR(1));
+
+switch (part) { 
+ 
+
+	case 1:
+         for(i=1;i<8;i++) { 
+            mvwprintw(w, i + loc, 14, "%s", "|"); 
+         }
+         break; 
+
+        case 2:
+           min = 1;
+           for(i=1;i<6;i++) {
+            mvwprintw(w, i + loc, 14 - min++, "%s", "/");
+         }
+         break;
+        
+        case 3:
+            min = 1;
+           for(i=1;i<6;i++) {
+            mvwprintw(w, i + loc, 14 + min++, "%s", "\\");
+         }
+         break;
+
+} 
+
+     wrefresh(w);
+      wattron(w, COLOR_PAIR(1));
+
+      return i + 1;
+
+}
 
 
-void main_menu(void) { 
+
+
+int main_menu(void) { 
     char manglyph[3][5] = {"  0"," /|\\"," / \\"}; 
     WINDOW *w;
-    char list[4][14] = { "New Game", "Load Game", "Player Stats", "Quit"};
-    char item[13];
+    char list[4][13] = { "New Game", "Load Game", "Stats", "Quit"};
+    char *item = malloc(14); 
     int ch, i = 0; 
+    int retval = 0; 
     int width = 15;
     int height = 10;
     int startx,starty; 
@@ -28,33 +84,39 @@ void main_menu(void) {
     for(i=0;i<4;i++) { 
           wattroff(w, A_STANDOUT);
            wattron(w, COLOR_PAIR(1)); 
-          sprintf(item, " %-12s", list[i]);
+          snprintf(item, 14," %-13s", list[i]);
           mvwprintw(w, i+1, 1, "%s", item);
+          memset(item,0,13); 
           }
-     sprintf(item, " %-12s", " ");
+
+ 
+     snprintf(item, 14," %-13s", "            ");
      mvwprintw(w, i+1, 1, "%s", item); 
-     for(i=0;i<=4;i++) {
+     for(i=0;i<3;++i) {
           wattroff(w, A_STANDOUT); 
            wattron(w, COLOR_PAIR(1));
-       sprintf(item, "   %-8s  ", manglyph[i]); 
+       snprintf(item,14, "   %-13s", manglyph[i]); 
        mvwprintw(w, i+6, 1, "%s", item);    
+       memset(item,0,13); 
     }
-
-     
+      //snprintf(item, 14," %-13s", "            ");
+      //mvwprintw(w, i+5, 1, "%s", item);
+   
     i = 0;
+    memset(item,0,13); 
     wattron(w, COLOR_PAIR(3));
     wattron(w, A_STANDOUT);
-    sprintf(item, " %-12s", list[i]);
+    snprintf(item,14," %-13s", list[i]);
     mvwprintw(w, i+1, 1, "%s", item);
     wattron(w, COLOR_PAIR(1));
     wattroff(w, A_STANDOUT); 
-    //wrefresh(w);
+    wrefresh(w);
     noecho(); 
     keypad(w, TRUE); 
     curs_set(0); 
     while(ch != 'q') {
        ch = wgetch(w); 
-       sprintf(item, " %-12s", list[i]); 
+       snprintf(item,14," %-13s", list[i]); 
        mvwprintw(w, i+1, 1, "%s", item); 
        switch(ch) { 
            case KEY_UP:
@@ -66,14 +128,15 @@ void main_menu(void) {
                i = (i > 3) ? 0: i; 
                break; 
             case 10:
-               if(i == 3)
+                  retval = i; 
                   ch = 'q';
                   break;
-
+                
+               
         }
         wattron(w, A_STANDOUT); 
          wattron(w, COLOR_PAIR(3));
-        sprintf(item, " %-12s", list[i]); 
+        snprintf(item,13," %-12s", list[i]); 
         mvwprintw(w, i+1, 1, "%s", item);
         
          wattron(w, COLOR_PAIR(3));
@@ -82,18 +145,133 @@ void main_menu(void) {
 	
 }
         delwin(w);
-        char* thi = getenv("HOME"); 
-        printf("%s\n",thi);  
+        refresh(); 
+        return retval; 
 }
+
 
 
 
 
 int main(void) { 
 
+int menuret = 0; 
+
 initscr(); 
 
-main_menu(); 
+menuret = main_menu(); 
+
+switch(menuret) { 
+
+case 0:
+   menuret = game_menu(); 
+   break; 
+case 3:
+   endwin(); 
+   break;
+}
+if(menuret == 1)
 endwin(); 
 
 }
+int game_menu(void) {
+    char manglyphs[6][5] = {"0"," /","|","\\","/","\\"};
+    WINDOW *w,*w2;
+    char list[4][13] = { "New Game", "Load Game", "Stats", "Quit"};
+    char *item = malloc(14);
+    int ch, i = 0;
+    int retval = 0;
+    int width = 30;
+    int height = 30;
+    int width2 = 70;
+    int height2 = 10;
+    int startx1,starty1,startx2,starty2;
+    start_color();
+    init_pair(1,COLOR_WHITE,COLOR_BLUE);
+    init_pair(2,COLOR_BLUE,COLOR_WHITE);
+    init_pair(3,COLOR_RED,COLOR_WHITE);
+    int loc,loc2; 
+    struct winsize max;
+    ioctl(0,TIOCGWINSZ, &max);
+    starty1 = max.ws_row / 5;
+    startx1 = max.ws_col / 4;
+    starty2 = max.ws_row - 10; 
+    startx2 = max.ws_col / 4;
+    w = newwin(height,width,starty1,startx1);
+    w2 = newwin(height2,width2,starty2,startx2);
+    wattron(w, COLOR_PAIR(1));
+    box(w,'+','=');
+    wattron(w2, COLOR_PAIR(1));
+    box(w2,'+','='); 
+    wrefresh(w); 
+    wrefresh(w2);
+    refresh();
+    sleep(1);
+    loc = draw_head(w);
+    loc2 = draw_part(w,1,loc);  
+    draw_part(w,2,loc);
+    draw_part(w,3,loc);
+    draw_part(w,2,loc2  + 3);
+    draw_part(w,3,loc2  +3);
+    wrefresh(w);
+    sleep(2);
+    delwin(w); 
+    delwin(w2); 
+    return 1; 
+}
+    /* wattron(w, COLOR_PAIR(1));
+    for(i=0;i<4;i++) {
+          wattroff(w, A_STANDOUT);
+           wattron(w, COLOR_PAIR(1));
+          snprintf(item, 14," %-13s", list[i]);
+          mvwprintw(w, i+1, 1, "%s", item);
+          memset(item,0,13);
+          }
+
+     snprintf(item, 14," %-13s", "            ");
+     mvwprintw(w, i+1, 1, "%s", item);
+     for(i=0;i<3;++i) {
+          wattroff(w, A_STANDOUT);
+           wattron(w, COLOR_PAIR(1));
+       snprintf(item,14, "   %-13s", manglyph[i]);
+       mvwprintw(w, i+6, 1, "%s", item);
+       memset(item,0,13);
+    }
+      //snprintf(item, 14," %-13s", "            ");
+      //mvwprintw(w, i+5, 1, "%s", item);
+
+    i = 0;
+    memset(item,0,13);
+    wattron(w, COLOR_PAIR(3));
+    wattron(w, A_STANDOUT);
+    snprintf(item,14," %-13s", list[i]);
+    mvwprintw(w, i+1, 1, "%s", item);
+    wattron(w, COLOR_PAIR(1));
+    wattroff(w, A_STANDOUT);
+    wrefresh(w);
+    noecho();
+    keypad(w, TRUE);
+    curs_set(0);
+    
+    while(ch != 'q') {
+       ch = wgetch(w);
+       snprintf(item,14," %-13s", list[i]);
+
+      }
+        wattron(w, A_STANDOUT);
+         wattron(w, COLOR_PAIR(3));
+        snprintf(item,13," %-12s", list[i]);
+        mvwprintw(w, i+1, 1, "%s", item);
+
+         wattron(w, COLOR_PAIR(3));
+         wattroff(w, A_STANDOUT);
+        wattron(w, COLOR_PAIR(1));
+
+}
+        delwin(w);
+        char* thi = getenv("HOME");
+        printf("%s\n",thi);
+        return retval;
+}
+
+*/
